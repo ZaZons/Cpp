e7d2677145974ea27f1842288c70735ccac9b7c9
ZaZons <tiago.ogomes.2004@gmail.com>
2021-11-10 10:42:00 +0000

    aula 10/11

---
+++ exemplos/exemploStringsCstyle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+	char apelido[20];
+	char nome[20];
+	char nomeCompleto[60];
+	char temp[60];
+
+	cout << nome << endl;
+	
+	char apelido[20] {};
+	char nome[20] {};
+	char nomeCompleto[60] {};
+	char temp[60] {};
+
+}
