3941315bf7099e90070cec3477701782cda1ea4e
ZaZons <tiago.ogomes.2004@gmail.com>
2022-02-03 12:34:32 +0000

    aula 03/02/22

--- exercicios/exercicioFuncoesPratica04.cpp
+++ exercicios/exercicioFuncoesPratica04.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 void mostrarMenu();
 void mostrarCabecalho();
-void mostrarAluno();
-
-
+void mostrarTodosAlunos(const int*, const int, const int);
+void mostrarAlunoPorId(int);
 
 int main() {
-	const size_t numAlunos{5}, numColunas{5}; 
+	const size_t numAlunos{4}, numColunas{5}; 
 	//campos:  ID do aluno, nota do Teste1, nota do Teste2, nota do período e nota final
+	int resposta{};
 	int alunos[numAlunos][numColunas]
 		{
 			{1232, 32, 34, 43, 43},
@@ -19,7 +19,23 @@ int main() {
 			{3456, 56, 34, 34, 56}
 		};
 
-	mostrarMenu();
+
+	do {
+		mostrarMenu();
+		
+		cin >> resposta;
+		switch(resposta) {
+			case 1:
+				mostrarTodosAlunos(alunos[0], numAlunos, numColunas);
+				break;
+			case 2:
+				int id{};
+				cout << "Por favor, introduza o ID do aluno: ";
+				cin >> id;
+				mostrarAlunoPorId(id);
+				break;
+		}
+	} while(resposta != 0);
 }
 
 void mostrarMenu() {
@@ -32,5 +48,29 @@ void mostrarMenu() {
 	cout << "3.Mostrar as notas mais elevadas e mais baixas" << endl;
 	cout << "0.Sair" << endl;
 
-	cout << "Introduza a sua escolha (0-3): "
+	cout << "Introduza a sua escolha (0-3): ";
+}
+
+void mostrarTodosAlunos(const int *alunos, const int nl, const int nc) {
+	mostrarCabecalho();	
+	for(size_t linhas = 0; linhas < nl; ++linhas) {
+		for(size_t colunas = 0; colunas < nc; ++colunas) {
+			cout << alunos[linhas * (nc) + colunas] << " \t\t  ";
+		}
+		cout << endl;
+	}
+}
+
+void mostrarAlunoPorId(const int id) {
+	mostrarCabecalho();
+}
+
+void mostrarCabecalho() {
+	cout << "=======================================================================" << endl;
+	cout << "ID Aluno";
+	cout << "\tTeste1";
+	cout << "\t\tTeste2"; 
+	cout << "\t\tPeriodo";
+	cout << "\t\tFinal" << endl;
+	cout << "=======================================================================" << endl;
 }
